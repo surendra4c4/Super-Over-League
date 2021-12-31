b751bfbf6d4cb70172b0080f37237937b353b71a
surendra4c4 <surendra4c4@gmail.com>
2021-12-31 18:13:00 +0000

    reactJS introduction

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div className="background-div">
+    <h1 className="heading">Super Over League</h1>
+    <div className="img-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="img-class"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="img-class"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
